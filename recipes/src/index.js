be5bfd70f6de57bfd37ab5652a341cb77b6d7917
emxxina <emxxina@gmail.com>
2020-12-14 02:52:47 +0100

    Morpheus React Tutorial 4 & 5

--- recipes/src/index.js
+++ recipes/src/index.js
@@ -4,10 +4,82 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.ticker = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ticker);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Aktuelle Uhrzeit: {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+
+class MagicButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {render: true};
+  }
+
+  deleteMe() {
+    this.setState({
+      render: false
+    });
+  }
+
+  render() {
+    if (this.state.render) {
+      return (
+          <button onClick={(e) => this.deleteMe(e)}>
+          Delete Me
+        </button>
+      );
+    } else {
+      return null;
+    } 
+  }
+}
+
+
+class MagicTable extends React.Component {
+  createTable() {
+    let table = [];
+    for (let i = 0; i < 10; i++) {
+      table.push(<tr><td><MagicButton/></td></tr>);
+    }
+    return table;
+  }
+
+  render() {
+    return(
+      <table>
+        {this.createTable()}
+      </table>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MagicTable/>,
   document.getElementById('root')
 );
 
