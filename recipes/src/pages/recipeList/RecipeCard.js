3636163b6b8f4d2bdea1e9b0f4f59b498eb8774f
emxxina <emxxina@gmail.com>
2020-12-24 21:52:30 +0100

    Added basic structure

---
+++ recipes/src/pages/recipeList/RecipeCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RecipeCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <h1>Title</h1>
+        );
+    }
+}
+
+export default RecipeCard;
