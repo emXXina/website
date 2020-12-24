8b03e8332c0b985050a4d9cd54fa7e7fc59560c3
emxxina <emxxina@gmail.com>
2020-12-24 21:40:12 +0100

    Created new react app for recipes

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
-
-import RecipeList from './pages/RecipeList/RecipeList';
-
 function App() {
   return (
-    <Router>
-      <Route path='/'>
-        <main>
-          <RecipeList/>
-        </main>
-      </Route>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
