a669ad4cd720f8a4800d3455e8e92f9c8c5a0e88
emxxina <emxxina@gmail.com>
2020-12-24 16:42:11 +0100

    Finshed Morpheus React Tutorial

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
 import Clock from './pages/Clock/clock';
 import CurrencyCalculator from './pages/CurrencyCalculator/CurrencyCalculator';
@@ -10,8 +10,10 @@ import MagicTable from './pages/MagicTable/MagicTable';
 function App() {
   return (
     <Router>
-      <Route path='/' exact  component={Clock} />
-      <Route path='/CurrencyCalculator' component={CurrencyCalculator} />
+      <Route path='/'>
+        {42 == 41 ? <Redirect to="/CurrencyCalculator/USD/EURO"/> : <Clock/>}
+      </Route>
+      <Route path={['/CurrencyCalculator/', '/CurrencyCalculator/:currency1/:currency2']} exact component={CurrencyCalculator} />
       <Route path={['/MagicTable', '/Table']} component={MagicTable} />
     </Router>
   );
--- recipes/src/pages/CurrencyCalculator/CurrencyCalculator.js
+++ recipes/src/pages/CurrencyCalculator/CurrencyCalculator.js
@@ -23,7 +23,7 @@ class CurrencyCalculator extends React.Component {
       
       let euro = 0;
       let dollar = 0;
-      if (currency == 'Euro') {
+      if (currency === 'Euro') {
         euro = value;
         dollar = 1.13 * value;
       } else {
@@ -31,11 +31,25 @@ class CurrencyCalculator extends React.Component {
         euro = 0.88 * value;
       }
   
+      const currency1 = this.props.match.params.currency1;
+      const currency2 = this.props.match.params.currency2;
+
       return(
-        <div>
-          <CurrencyInput currency="Euro" value={euro} onCurrencyChange={this.handleEuroChange}/>
-          <CurrencyInput currency="Dollar" value={dollar} onCurrencyChange={this.handleDollarChange}/>
-        </div>
+            <div>
+                <CurrencyInput currency="Euro" value={euro} onCurrencyChange={this.handleEuroChange}/>
+                <CurrencyInput currency="Dollar" value={dollar} onCurrencyChange={this.handleDollarChange}/>
+
+                { currency1 != null && currency2 != null ?
+                  [
+                    <h1>
+                        Currency1: {currency1}
+                    </h1>,                    
+                    <h1>
+                        Currency2: {currency2}
+                    </h1>
+                  ] : null
+                }
+            </div>
       );
     }
 }
