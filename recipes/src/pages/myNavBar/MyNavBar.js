1e97172c7b3c34c94503bf235b9084612f0f1ff1
emxxina <emxxina@gmail.com>
2020-12-25 21:16:47 +0100

    Made navbar change size when scolling

--- recipes/src/pages/myNavBar/MyNavBar.js
+++ recipes/src/pages/myNavBar/MyNavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Divider } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Divider, Typography } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import TuneIcon from '@material-ui/icons/Tune';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -8,16 +8,44 @@ import './mynavbar.scss';
 import logo from './Muffin_200.png';
 
 class MyNavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {small: false};
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll(event) {
+        let scrollTop = window.scrollY;
+
+        if (scrollTop > 100) {
+            this.setState({ small: true });
+            document.getElementById("toolbar").style.margin = "0 0";    
+        } else {
+            this.setState({ small: false });
+            document.getElementById("toolbar").style.margin = "1rem 0"; 
+        }
+        
+    }
+
     render() {
         return(
             <AppBar position="fixed">
-                <Toolbar className="toolbar">
+                <Toolbar className="toolbar" id="toolbar">
                     <div className="toolbar__subbar">
                         <img src={logo} alt="Logo"/>
-                        <h4>finnupa.de</h4>
+                        <Typography variant="subtitle1">finnupa.de</Typography>
                     </div>
 
-                    <h1>Rezepte</h1>
+                    <Typography variant={this.state.small ? "h4" : "h1"} component="h1" id="title">Rezepte</Typography>
 
                     <div className="toolbar__subbar">
                         <IconButton color="inherit">
