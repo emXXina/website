4d512402402964860a0455d11cc1cafadb7e6945
emxxina <emxxina@gmail.com>
2020-12-26 16:00:47 +0100

    Implemented Typography in RecipeCard

--- recipes/src/pages/recipeList/RecipeCard.js
+++ recipes/src/pages/recipeList/RecipeCard.js
@@ -4,7 +4,7 @@ import '@material/react-card/dist/card.css';
 import "./recipecard.scss";
 import defaultImg from './default.jpg';
 
-import { Card, CardHeader, CardMedia, CardContent, CardActionArea, CardActions} from '@material-ui/core';
+import { Card, CardHeader, CardMedia, CardContent, CardActionArea, CardActions, Typography} from '@material-ui/core';
 import { Button, IconButton } from '@material-ui/core/';
 import ShareIcon from '@material-ui/icons/Share';
 import FavoriteIcon from '@material-ui/icons/Favorite';
@@ -20,17 +20,13 @@ class RecipeCard extends React.Component {
     constructor(props) {
         super(props);
         
-        this.title = "";
-        (this.props.title == null) ? this.title = "Ohne Titel" : this.title = this.props.title;
+        this.title = (this.props.title == null) ? "Ohne Titel" : this.props.title;
 
-        this.labels = "";
-        (this.props.labels == null) ? this.labels = "Keine Label" : this.labels = this.props.labels;
+        this.labels = (this.props.labels == null) ? "Keine Label" : this.props.labels;
 
-        this.img = "";
-        (this.props.img == null) ? this.img = defaultImg : this.img = this.props.img;
+        this.img = (this.props.img == null) ? defaultImg : this.props.img;
 
-        this.description = "";
-        (this.props.description == null) ? this.description = "Keine Beschreibung" : this.description = this.props.description;
+        this.description = (this.props.description == null) ? "Keine Beschreibung" : this.props.description;
     
         this.handleTogglePlay = this.handleTogglePlay.bind(this);
     }
@@ -41,10 +37,13 @@ class RecipeCard extends React.Component {
         return(
             <Card variant="outlined" >
                 <CardActionArea>
-                    <CardHeader title={this.title} subheader={this.labels}/>
+                    <CardContent>
+                        <Typography variant="h4" component="h2">{this.title}</Typography>
+                        <Typography variant="h6">{this.labels}</Typography>
+                    </CardContent>
                     <CardMedia image={this.img} title="Test" className="media" />
                     <CardContent>
-                        <p>{this.description}</p>
+                        <Typography variant="body2">{this.description}</Typography>
                     </CardContent>
                 </CardActionArea>
 
