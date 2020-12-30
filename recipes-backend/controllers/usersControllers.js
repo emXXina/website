0c5b951ff502539524657f0d8d80ce2dd34722d4
emxxina <emxxina@gmail.com>
2020-12-30 20:48:51 +0100

    deletes users from backend

--- recipes-backend/controllers/usersControllers.js
+++
@@ -1,5 +0,0 @@
-exports.usersController = (req, res) => {
-    res.json({
-        usersList: ["user1", "user2"]
-    })
-}
--- recipes-backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-usersRoute = require("../controllers/usersControllers");
-
-/* GET users listing. */
-router.get('/', usersRoute.usersController);
-
-module.exports = router;
