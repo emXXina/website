4f176996948044799c072a4b1219d1e72906f755
emxxina <emxxina@gmail.com>
2021-02-13 14:24:16 +0100

    starts recipe creator form

--- recipes/src/components/create/AddFundamentals.js
+++ recipes/src/components/create/AddFundamentals.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { TextField } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 export default function AddFundamentals() {
+
+    const theme = useTheme();
+    const useStyles = makeStyles({
+        form: {
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                marginLeft: 0,
+                textAlign: 'center'
+            }
+        }
+    })
+    const classes = useStyles();
+
     return(
-        <p>test</p>
+        <div>
+            <form className={classes.form} autoComplete='off'>
+                <TextField
+                    variant="filled"
+                    id="name"
+                    label="Name" />
+                <TextField
+                    className={classes.fullWidth}
+                    fullWidth
+                    variant="filled"
+                    id="description"
+                    label="Kurzbeschreibung" />
+            </form>
+        </div>
     );
 }
