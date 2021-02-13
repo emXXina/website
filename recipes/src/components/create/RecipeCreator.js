ed93222e46f316a47d96a053e2c88069b4122fce
emxxina <emxxina@gmail.com>
2021-02-13 15:24:07 +0100

    designs general structure of creator steps

--- recipes/src/components/create/RecipeCreator.js
+++ recipes/src/components/create/RecipeCreator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, Button, Typography, CardContent, Stepper, Step, StepLabel } from '@material-ui/core';
+import { Card, Button, Typography, CardContent, Stepper, Step, StepLabel, Divider } from '@material-ui/core';
 import AddFundamentals from './AddFundamentals';
 
 export default function RecipeCreator() {
@@ -62,23 +62,43 @@ export default function RecipeCreator() {
                         );
                     })}
                 </Stepper>
+            </CardContent>
 
+            <Divider/>
+            
+            <CardContent>
                 <div>
                     {activeStep === steps.length ? (
-                        <Typography>Fertig!</Typography>
+                        <div>
+                            <Typography variant="h4" component="h3" paragraph>Fertig?</Typography>
+                        
+                            <Typography>Wenn du sicher bist, dass du wirklich fertig bist, drücke auf Fertig.</Typography>
+                        </div>
                     ) : (
                         <div>
                             <Typography variant="h4" component="h3" paragraph>{steps[activeStep]}</Typography>
+                            
                             {getContent(activeStep)}
-                            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
-                                <Button disabled={activeStep === 0} onClick={handleBack}>
-                                    Zurück
-                                </Button>
-                                <Button variant="contained" color="primary" onClick={handleNext}>
-                                    Weiter
-                                </Button>
-                            </div>
                         </div>
+                    )}
+                </div>
+            </CardContent>
+            
+            <Divider/>
+
+            <CardContent>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
+                    <Button disabled={activeStep === 0} onClick={handleBack}>
+                        Zurück
+                    </Button>
+                    {activeStep === steps.length ? (
+                        <Button variant="contained" color="primary" onClick={handleNext}>
+                            Fertig
+                        </Button>
+                    ) : (
+                        <Button variant="contained" color="primary" onClick={handleNext}>
+                            Weiter
+                        </Button>
                     )}
                 </div>
             </CardContent>
