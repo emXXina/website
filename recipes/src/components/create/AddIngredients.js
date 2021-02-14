201a6560620c4d12c5be6d86671c9432486f14f1
emxxina <emxxina@gmail.com>
2021-02-14 03:53:33 +0100

    adds form to enter ingredients

--- recipes/src/components/create/AddIngredients.js
+++ recipes/src/components/create/AddIngredients.js
@@ -1,9 +1,63 @@
+import React, {useState} from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import Ingredient from "./Ingredient";
+import BasicBigButton from "../utils/BasicBigButton";
+import AddIcon from '@material-ui/icons/Add';
+import { GridList, GridListTile } from '@material-ui/core';
 
 function AddIngredients() {
+    const useStyles = makeStyles({
+        tile: {
+            marginBottom: '1rem',
+        }
+    });
+    const classes = useStyles();
+
+    // add all available ingredient categories to this state
+    const units = ["", "EL", "TL", "ml", "l", "mg", "g", "kg", "Tropfen", "Prise(n)", "Pck", "Scheibe(n)", "Tasse(n)", "Pfund"];
+    const [categories, setCategories] = useState(["---", "Teig"]);
+    const basicIngredient = {name: '', unit: units[0], quantity: '0', category_name: categories[0]};
+    const [ingredients, setIngredients] = useState([basicIngredient]);
+
+    const setIngredient = (idx, attribute, value) => {
+        console.log(ingredients);
+        console.log(idx);
+        console.log(attribute);
+        console.log(value);
+        ingredients[idx] = {
+            ...ingredients[idx],
+            [attribute]: value
+        };
+        setIngredients(ingredients.slice());
+    }
+
+    const getIngredient = (idx) => {
+        return ingredients[idx];
+    }
+                                            
+    const addIngredient = () => {
+        ingredients.push(basicIngredient);
+        setIngredients(ingredients.slice());
+    };
+
+    const getTiles = () => {
+        var tiles = [];
+        for (var i = 0; i < ingredients.length; i++) {
+            tiles.push(
+                <GridListTile key={i} className={classes.tile}>
+                    <Ingredient categories={categories} units={units} idx={i} setIngredient={setIngredient} getIngredient={getIngredient}/>
+                </GridListTile>
+                );
+        }
+        return tiles;
+    };
+
     return(
         <form>
-            <Ingredient/>
+            <GridList cellHeight="auto" cols={1} spacing={0}>
+                {getTiles()}
+            </GridList>
+            <BasicBigButton icon={<AddIcon/>} action={{onClick: addIngredient}} />
         </form>
     );
 }
--- recipes/src/components/create/Ingredient.js
+++ recipes/src/components/create/Ingredient.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { MenuItem, TextField } from "@material-ui/core";
 
-export default function Ingredient() {
+export default function Ingredient(props) {
     const theme = useTheme();
     const useStyles = makeStyles({
         container: {
@@ -12,13 +13,86 @@ export default function Ingredient() {
             padding: '.4rem',
             background: theme.palette.background.default,
             display: 'block',
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                marginLeft: 0
+            },
+            '& .MuiSelect-root': {
+                width: '200px'
+            }
+        },
+        numberInput: {
+            width: '7rem',
         }
     })
     const classes = useStyles();
 
+    const idx = props.idx;
+    const categories = useRef(props.categories);
+    const units = useRef(props.units);
+    const getIngredient = props.getIngredient;
+    const setIngredient = props.setIngredient;
+    const [isQuantityValid, setIsQuantityValid] = useState(true);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        if (name === "quantity" && value < 0) {
+            setIsQuantityValid(false);
+        } else {
+            setIngredient(idx, name, value);
+
+            if (name === "quantity") {
+                setIsQuantityValid(true);
+            }
+        }
+    }
+
     return(
         <div className={classes.container}>
-
+            <TextField
+                name="name"
+                required
+                variant="outlined"
+                label="Zutatenbezeichnung"
+                onChange={handleChange}
+                value={getIngredient(idx).name}
+            /><br/>
+            <TextField
+                name="category_name"
+                select
+                variant="outlined"
+                label="Zutatenkategorie"
+                onChange={handleChange}
+                value={getIngredient(idx).category_name}
+            >
+                {categories.current.map((category, idx) => (
+                    <MenuItem value={category} key={idx}>{category}</MenuItem>
+                ))} 
+            </TextField><br/>
+            <TextField className={classes.numberInput}
+                error={!isQuantityValid}
+                helperText={isQuantityValid ? "" : "Die Menge muss positiv sein."}
+                type="number"
+                name="quantity"
+                variant="outlined"
+                label="Menge"
+                onChange={handleChange}
+                value={getIngredient(idx).quantity}
+            />
+            <TextField
+                name="unit"
+                select
+                variant="outlined"
+                label="Einheit"
+                onChange={handleChange}
+                value={getIngredient(idx).unit}
+            >
+                {units.current.map((unit, idx) => (
+                    <MenuItem value={unit} kex={idx}>{unit}</MenuItem>
+                ))}
+            </TextField>
         </div>
     );
 }
