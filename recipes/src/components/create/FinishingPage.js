a431ca9ba7a06eb1828bfa83c1cddcb98e14ca3d
emxxina <emxxina@gmail.com>
2021-02-14 03:53:13 +0100

    redesignes general recipe creator interface

---
+++ recipes/src/components/create/FinishingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Typography} from "@material-ui/core";
+
+export default function FinishingPage() {
+    return(
+        <div>
+            <Typography>Wenn du sicher bist, dass du wirklich fertig bist, drücke auf Fertig.</Typography>
+        </div>
+    );
+}
--- recipes/src/components/create/RecipeCreator.js
+++ recipes/src/components/create/RecipeCreator.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Card, Button, Typography, CardContent, Stepper, Step, StepLabel, Divider } from '@material-ui/core';
+import { Card, Button, Typography, CardContent, MobileStepper, Divider, makeStyles, useTheme  } from '@material-ui/core';
 import AddFundamentals from './AddFundamentals';
 import AddIngredients from './AddIngredients';
+import FinishingPage from './FinishingPage';
 
 export default function RecipeCreator() {
-    const steps = ['Grundlegende Eigenschaften', 'Zutaten', 'Zubereitung'];
-    const [activeStep, setActiveStep] = useState(0);
-    const [completed, setCompleted] = useState(new Set());
+    const steps = ['Grundlegende Eigenschaften', 'Zutaten', 'Zubereitung', 'Fertig?'];
+    const [activeStep, setActiveStep] = useState(1);
     
     function getContent(step) {
         switch(step) {
@@ -16,73 +16,85 @@ export default function RecipeCreator() {
                 return <AddIngredients/>;
             case 2:
                 return "Zubereitung...";
+            case 3:
+                return <FinishingPage/>;
             default:
                 return "Das sollte nicht passieren.";
         }
     };
-    
-    const isCompleted = (index) => {
-        return completed.has(index);
-    };
 
-    const isNowComplete = () => {
-        return true;
-    };
+    function NextButton() {
+        return(
+            <div>
+                {activeStep === (steps.length - 1) ? (
+                    <Button variant="contained" color="primary" onClick={handleNext}>
+                        Fertig
+                    </Button>
+                ) : (
+                    <Button variant="contained" color="primary" onClick={handleNext}>
+                        Weiter
+                    </Button>
+                )}
+            </div>
+        );
+    }
+
+    function BackButton() {
+        return(
+            <Button disabled={activeStep === 0} onClick={handleBack}>
+                Zurück
+            </Button>
+        );
+    }
 
     const handleNext = () => {
-        if (isNowComplete()) {
-            setCompleted((prevCompleted) => {
-                const newCompleted = new Set(prevCompleted.values());
-                newCompleted.add(activeStep);
-                return newCompleted;
-            });
-        }
-        
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        console.log(completed);
     };
 
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
+    const theme = useTheme();
+    const useStyles = makeStyles({
+        stepper: {
+            padding: 0,
+            backgroundColor: theme.palette.common.white
+        }
+    });
+    const classes = useStyles();
+
     return(
         <Card className="big-card">
             <CardContent>
                 <Typography variant="h2">Neues Rezept</Typography>
+            </CardContent>
 
-                <Stepper activeStep={activeStep} alternativeLabel>
-                    {steps.map((label, index) => {
-                        const stepProps = {};
-                        if(!isCompleted(index) || index === activeStep) {
-                            stepProps.completed = false;
-                        }
-                        return(
-                            <Step key={label} {...stepProps}>
-                                <StepLabel>{label}</StepLabel>
-                            </Step>
-                        );
-                    })}
-                </Stepper>
+            <Divider/>
+
+            <CardContent>
+                <MobileStepper 
+                    variant="dots"
+                    className={classes.stepper}
+                    steps={steps.length}
+                    position="static"
+                    activeStep={activeStep}
+                    nextButton={
+                        <NextButton />
+                    }
+                    backButton={
+                        <BackButton />
+                    }
+                />
             </CardContent>
 
             <Divider/>
             
             <CardContent>
                 <div>
-                    {activeStep === steps.length ? (
-                        <div>
-                            <Typography variant="h4" component="h3" paragraph>Fertig?</Typography>
-                        
-                            <Typography>Wenn du sicher bist, dass du wirklich fertig bist, drücke auf Fertig.</Typography>
-                        </div>
-                    ) : (
-                        <div>
-                            <Typography variant="h4" component="h3" paragraph>{steps[activeStep]}</Typography>
-                            
-                            {getContent(activeStep)}
-                        </div>
-                    )}
+                    <Typography variant="h4" component="h3" paragraph>{steps[activeStep]}</Typography>
+                    
+                    {getContent(activeStep)}
                 </div>
             </CardContent>
             
@@ -90,18 +102,8 @@ export default function RecipeCreator() {
 
             <CardContent>
                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
-                    <Button disabled={activeStep === 0} onClick={handleBack}>
-                        Zurück
-                    </Button>
-                    {activeStep === steps.length ? (
-                        <Button variant="contained" color="primary" onClick={handleNext}>
-                            Fertig
-                        </Button>
-                    ) : (
-                        <Button variant="contained" color="primary" onClick={handleNext}>
-                            Weiter
-                        </Button>
-                    )}
+                    <BackButton />
+                    <NextButton />
                 </div>
             </CardContent>
         </Card>
